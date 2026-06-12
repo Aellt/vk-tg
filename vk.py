31d3068fefdbeb42e3e162634c69ec0ba2023e55
Aellt <artemaellt@gmail.com>
2026-06-12 12:16:18 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -3,7 +3,15 @@ import requests
 VK_API = "https://api.vk.com/method/wall.get"
 VERSION = "5.131"
 
+def is_valid_post(post):
+    if post.get("is_pinned", 0) == 1:
+        return False
 
+    if post.get("marked_as_ads", 0) == 1:
+        return False
+
+    return True
+    
 def get_latest_posts(domain, token, count=3):
     r = requests.get(VK_API, params={
         "access_token": token,
