0e9cbd81bcd4e08b3d48902c55ef2dfb58bca188
Aellt <artemaellt@gmail.com>
2026-06-12 12:23:58 +0300

    Create state.py

---
+++ state.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+STATE_FILE = "state.json"
+
+
+def load_state():
+    if not os.path.exists(STATE_FILE):
+        return {"sent": []}
+
+    with open(STATE_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_state(state):
+    with open(STATE_FILE, "w") as f:
+        json.dump(state, f)
